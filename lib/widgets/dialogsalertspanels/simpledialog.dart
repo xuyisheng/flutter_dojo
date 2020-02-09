14f3d93d2db3421ee3b1fc8b458f2fc52b2243eb
xuyisheng <xuyisheng89@163.com>
2020-02-09 15:32:40 +0800

    SimpleDialog增加contentpadding

--- lib/widgets/dialogsalertspanels/simpledialog.dart
+++ lib/widgets/dialogsalertspanels/simpledialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dojo/common/main_title_widget.dart';
+import 'package:flutter_dojo/common/subtitle_widget.dart';
 
 class SimpleDialogWidget extends StatelessWidget {
   @override
@@ -10,7 +11,12 @@ class SimpleDialogWidget extends StatelessWidget {
         RaisedButton(
           onPressed: () => showSimpleDialog(context),
           child: Text('Show SimpleDialog'),
-        )
+        ),
+        SubtitleWidget('SimpleDialog with ContentPadding'),
+        RaisedButton(
+          onPressed: () => showSimpleDialog2(context),
+          child: Text('Show SimpleDialog'),
+        ),
       ],
     );
   }
@@ -21,6 +27,28 @@ class SimpleDialogWidget extends StatelessWidget {
       builder: (BuildContext context) {
         return SimpleDialog(
           title: Text('SimpleDialog title'),
+          children: <Widget>[
+            SimpleDialogOption(
+              child: Text('item 1'),
+              onPressed: () {},
+            ),
+            SimpleDialogOption(
+              child: Text('item 2'),
+              onPressed: () {},
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showSimpleDialog2(BuildContext context) {
+    showDialog<Null>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Text('SimpleDialog title'),
+          contentPadding: EdgeInsets.all(32),
           children: <Widget>[
             SimpleDialogOption(
               child: Text('item 1'),

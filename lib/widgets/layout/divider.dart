98d04c0db6ffe60d992a4696c99b8ec17a642619
xuyisheng <xuyisheng89@163.com>
2020-04-11 10:18:29 +0800

    update divider

--- lib/widgets/layout/divider.dart
+++ lib/widgets/layout/divider.dart
@@ -4,29 +4,33 @@ import 'package:flutter_dojo/common/main_title_widget.dart';
 class DividerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        MainTitleWidget('Divider基本用法'),
-        ListTile(
-          title: Text('Item1'),
-        ),
-        Divider(),
-        ListTile(
-          title: Text('Item2'),
-        ),
-        Divider(),
-        ListTile(
-          title: Text('Item3'),
-        ),
-        Divider(),
-        MainTitleWidget('Divider的高度范围 height'),
-        Divider(height: 60),
-        MainTitleWidget('Divider左侧的空间大小 indent'),
-        Divider(indent: 100),
-        MainTitleWidget('Divider颜色 color'),
-        Divider(color: Colors.red),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          MainTitleWidget('Divider基本用法'),
+          ListTile(
+            title: Text('Item1'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Item2'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Item3'),
+          ),
+          Divider(),
+          MainTitleWidget('Divider的高度范围 height'),
+          Divider(height: 60),
+          MainTitleWidget('Divider的分割线粗细'),
+          Divider(thickness: 20),
+          MainTitleWidget('Divider左侧的空间大小 indent'),
+          Divider(indent: 100),
+          MainTitleWidget('Divider颜色 color'),
+          Divider(color: Colors.red),
+        ],
+      ),
     );
   }
 }

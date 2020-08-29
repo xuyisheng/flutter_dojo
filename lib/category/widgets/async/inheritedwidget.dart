1c43d6a1d78a711fe2251eee837f22cd0e704fd4
xuyisheng <xuyisheng89@163.com>
2020-08-29 17:01:20 +0800

    update

--- lib/category/widgets/async/inheritedwidget.dart
+++ lib/category/widgets/async/inheritedwidget.dart
@@ -1,53 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dojo/common/subtitle_widget.dart';
 
-class InheritedWidgetWidget extends StatefulWidget {
+class InheritedWidgetWidget extends StatelessWidget {
   @override
-  _InheritedWidgetWidgetState createState() => _InheritedWidgetWidgetState();
+  Widget build(BuildContext context) {
+    return RootContainer(
+      child: Column(
+        children: <Widget>[
+          Widget1(),
+          Widget2(),
+          Widget3(),
+        ],
+      ),
+    );
+  }
 }
 
-class _InheritedWidgetWidgetState extends State<InheritedWidgetWidget> {
+class Widget1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('build Widget1');
+    return SubtitleWidget('InheritedWidget本身不具有写数据的功能，需要结合State来获取数据修改的能力');
+  }
+}
+
+class Widget2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('build Widget2');
+    return Text(
+      'show ${RootContainer.of(context).count}',
+      style: TextStyle(fontSize: 20),
+    );
+  }
+}
+
+class Widget3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('build Widget3');
+    return RaisedButton(
+      onPressed: () {
+        RootContainer.ofNoBuild(context).incrementCounter();
+      },
+      child: Text('Add'),
+    );
+  }
+}
+
+class RootContainer extends StatefulWidget {
+  final Widget child;
+
+  RootContainer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  _RootContainerState createState() => _RootContainerState();
+
+  static _RootContainerState of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<Root>().state;
+
+  static _RootContainerState ofNoBuild(BuildContext context) => context.findAncestorWidgetOfExactType<Root>().state;
+}
+
+class _RootContainerState extends State<RootContainer> {
   int count = 0;
 
   void incrementCounter() => setState(() => count++);
 
   @override
   Widget build(BuildContext context) {
-    return Root(
-      state: this,
-      child: Child(),
-    );
-  }
-}
-
-class Child extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    debugPrint('build');
-    Root root = Root.of(context);
-    return Column(
-      children: <Widget>[
-        SubtitleWidget('InheritedWidget本身不具有写数据的功能，需要结合State来获取数据修改的能力'),
-        Text(
-          'show ${root.state.count}',
-          style: TextStyle(fontSize: 20),
-        ),
-        RaisedButton(
-          onPressed: () {
-            root.state.incrementCounter();
-          },
-          child: Text('Add'),
-        ),
-      ],
-    );
+    return Root(state: this, child: widget.child);
   }
 }
 
 // 同时支持读取和写入
 class Root extends InheritedWidget {
-  static Root of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<Root>();
-
-  final _InheritedWidgetWidgetState state;
+  final _RootContainerState state;
 
   Root({
     Key key,
@@ -57,7 +87,34 @@ class Root extends InheritedWidget {
 
   // 判断是否需要更新
   @override
-  bool updateShouldNotify(Root oldWidget) => state.count != oldWidget.state.count;
+  bool updateShouldNotify(Root oldWidget) => true;
+}
+
+class InheritedWidgetReadOnlyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ReadOnlyRoot(
+      count: 1008,
+      child: ChildReadOnly(),
+    );
+  }
+}
+
+class ChildReadOnly extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('build');
+    ReadOnlyRoot root = ReadOnlyRoot.of(context);
+    return Column(
+      children: <Widget>[
+        SubtitleWidget('InheritedWidget本身不具有写数据的功能，需要结合State来获取数据修改的能力'),
+        Text(
+          'show ${root.count}',
+          style: TextStyle(fontSize: 20),
+        ),
+      ],
+    );
+  }
 }
 
 // 仅支持读取属性

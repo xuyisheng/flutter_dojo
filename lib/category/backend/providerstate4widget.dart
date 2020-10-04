ca4ba7068cc32ed51baf5eeaeea76d10fa1e522d
xuyisheng <xuyisheng89@163.com>
2020-10-04 10:57:34 +0800

    update provider2

--- lib/category/backend/providerstate4widget.dart
+++ lib/category/backend/providerstate4widget.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dojo/common/main_title_widget.dart';
+import 'package:provider/provider.dart';
 
 class ProviderState4Widget extends StatefulWidget {
   @override
@@ -9,17 +10,7 @@ class ProviderState4Widget extends StatefulWidget {
 }
 
 class _ProviderState4WidgetState extends State<ProviderState4Widget> {
-  DataModel data = DataModel();
   int pageIndex = 0;
-  int checkedCount = 0;
-
-  @override
-  void initState() {
-    data.getData(pageIndex).then((value) {
-      setState(() => data.dataList = value);
-    });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,72 +18,85 @@ class _ProviderState4WidgetState extends State<ProviderState4Widget> {
       appBar: AppBar(title: Text('Provider')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            MainTitleWidget('刷新 加载更多 收藏'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                RaisedButton(
-                  onPressed: () {
-                    setState(() => data.dataList.clear());
-                    data.getData(0).then((value) {
-                      setState(() => data.dataList = value);
-                    });
-                  },
-                  child: Text('Refresh'),
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    data.getData(++pageIndex).then((value) {
-                      setState(() => data.dataList = value);
-                    });
-                  },
-                  child: Text('Load More'),
-                ),
-                Text('Checked Count $checkedCount'),
-              ],
-            ),
-            Expanded(
-              child: data.dataList.length > 0
-                  ? ListView.builder(
-                      itemBuilder: (context, index) {
-                        var itemModel = data.dataList[index];
-                        return Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Checkbox(
-                                    value: itemModel.isCheck,
-                                    onChanged: (flag) {
-                                      setState(() {
-                                        var isCheck = itemModel.isCheck;
-                                        if (isCheck) {
-                                          checkedCount--;
-                                        } else {
-                                          checkedCount++;
-                                        }
-                                        return itemModel.isCheck = !isCheck;
-                                      });
-                                    }),
-                                Text(itemModel.title),
-                                Spacer(),
-                                Icon(Icons.favorite),
-                                ConstrainedBox(
-                                  constraints: BoxConstraints(minWidth: 30),
-                                  child: Center(child: Text(itemModel.likeCount.toString())),
+        child: ChangeNotifierProvider(
+          create: (BuildContext context) {
+            var model = DataModel();
+            model.getData(pageIndex);
+            return model;
+          },
+          child: Column(
+            children: [
+              MainTitleWidget('刷新 加载更多 收藏'),
+              Builder(
+                builder: (BuildContext context) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      RaisedButton(
+                        onPressed: () {
+                          var model = Provider.of<DataModel>(context, listen: false);
+                          model.refreshData();
+                          model.getData(0);
+                        },
+                        child: Text('Refresh'),
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          context.read<DataModel>().getData(++pageIndex);
+                        },
+                        child: Text('Load More'),
+                      ),
+                      Text('Checked Count ${context.watch<DataModel>().checkedCount}'),
+                    ],
+                  );
+                },
+              ),
+              Expanded(
+                child: Selector<DataModel, DataModel>(
+                  shouldRebuild: (pre, next) => pre.shouldListRebuild,
+                  selector: (context, dataModel) => dataModel,
+                  builder: (BuildContext context, DataModel dataModel, Widget child) {
+                    if (dataModel.dataList.length > 0) {
+                      return ListView.builder(
+                        itemBuilder: (context, index) {
+                          return Selector<DataModel, ItemModel>(
+                            selector: (context, value) => value.dataList[index],
+                            builder: (BuildContext context, data, Widget child) {
+                              debugPrint(('Item $index rebuild'));
+                              return Card(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Checkbox(
+                                          value: data.isCheck,
+                                          onChanged: (flag) {
+                                            dataModel.updateChecked(index, !data.isCheck);
+                                          }),
+                                      Text(data.title),
+                                      Spacer(),
+                                      Icon(Icons.favorite),
+                                      ConstrainedBox(
+                                        constraints: BoxConstraints(minWidth: 30),
+                                        child: Center(child: Text(data.likeCount.toString())),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                      itemCount: data.dataList.length,
-                    )
-                  : Center(child: CircularProgressIndicator()),
-            ),
-          ],
+                              );
+                            },
+                          );
+                        },
+                        itemCount: dataModel.dataList.length,
+                      );
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -107,20 +111,42 @@ class ItemModel {
   ItemModel(this.title, this.isCheck, this.likeCount);
 }
 
-class DataModel {
+class DataModel with ChangeNotifier {
   List<ItemModel> dataList = List();
+  int checkedCount = 0;
+  bool shouldListRebuild = true;
 
-  Future<List<ItemModel>> getData(int pageIndex) async {
+  getData(int pageIndex) async {
     List<ItemModel> items = await api.getListDataOfIndex(pageIndex);
+    shouldListRebuild = true;
     dataList.addAll(items);
-    return dataList;
+    notifyListeners();
+  }
+
+  refreshData() {
+    dataList.clear();
+    checkedCount = 0;
+    shouldListRebuild = true;
+    notifyListeners();
+  }
+
+  updateChecked(int index, bool isChecked) {
+    shouldListRebuild = false;
+    var item = dataList[index];
+    if (isChecked) {
+      checkedCount++;
+    } else {
+      checkedCount--;
+    }
+    dataList[index] = ItemModel(item.title, isChecked, item.likeCount);
+    notifyListeners();
   }
 }
 
 class API {
   Future<List<ItemModel>> getListDataOfIndex(int pageIndex) async {
     List<ItemModel> data = List();
-    await Future.delayed(Duration(seconds: 3));
+    await Future.delayed(Duration(seconds: 2));
     List.generate(
       10,
       (index) => (data.add(ItemModel('Title $index  @Page $pageIndex', false, Random().nextInt(20) + 1))),
